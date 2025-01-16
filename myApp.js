9f34ac6bab623b49ed9c2147719543daf357c700
Rubenpau <rubenpaul337@gmail.com>
2025-01-16 11:03:56 +0530

    modified

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require('helmet'); 
 const app = express();
-const PORT = process.env.PORT || 3030;
+
 
 
 
@@ -58,5 +58,5 @@ app.get("/", function (request, response) {
 });
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`server started on port ${port}`);
 });
